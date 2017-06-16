64f68065ef5772bef9f055455bbfa78ece4ed1a0
NessCollege <nesscollege2017@gmail.com>
2017-06-16 13:03:19 +0300

    Chat Adapter

--- app/src/main/java/ness/edu/firebasedatabase/WhatsappFragment.java
+++ app/src/main/java/ness/edu/firebasedatabase/WhatsappFragment.java
@@ -3,6 +3,7 @@ package ness.edu.firebasedatabase;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
@@ -11,14 +12,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,7 +63,7 @@ public class WhatsappFragment extends Fragment {
         mDatabase = FirebaseDatabase.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
 
-        readFromDb();
+        setupRecycler();
         return view;
     }
 
@@ -83,15 +94,33 @@ public class WhatsappFragment extends Fragment {
     }
 
 
+    private void setupRecycler() {
+        ChatAdapter adapter = new ChatAdapter(mDatabase.getReference("Chat"));
+        rvChat.setAdapter(adapter);
+        rvChat.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+
     private void readFromDb(){
         //1) get a ref to the table.
         DatabaseReference chatRef = mDatabase.getReference("Chat");
-
+        //final List<String> items = new ArrayList<>();
         //2) add a listener to the table
+
+
+
+        //get the table at the beginning
+        //AND each time the data changes - GET ALL THE TABLE AGAIN.
         chatRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.d("Ness", dataSnapshot.toString());
+                Iterable<DataSnapshot> rows = dataSnapshot.getChildren();
+
+                for (DataSnapshot row : rows) {
+                    String text = row.getValue(String.class);
+                   // items.add(text);
+                    Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
@@ -99,5 +128,82 @@ public class WhatsappFragment extends Fragment {
 
             }
         });
+    }
+
+
+    private void readOnce(){
+        //get a reference to the table.
+        //add a listener.
+
+        //Get the data once from the server. Not updating unless we run the query again.
+        mDatabase.getReference("Chat").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot row : dataSnapshot.getChildren()) {
+                    Toast.makeText(getContext(), row.getValue(String.class), Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private void readIncremental(){
+
+
+        mDatabase.getReference("Chat").addChildEventListener(new ChildEventListener() {
+
+            //Once get all the table:
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String key) {
+                //Once get all the table:
+                String text = dataSnapshot.getValue(String.class);
+                Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+                //once a new item is added we will only get the new child
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    static class ChatAdapter extends FirebaseRecyclerAdapter<String, ChatAdapter.ChatViewHolder>{
+        public ChatAdapter(Query ref) {
+            super(String.class, R.layout.chat_item, ChatViewHolder.class, ref);
+        }
+        @Override
+        protected void populateViewHolder(ChatViewHolder v, String text, int position) {
+            v.tvChat.setText(text);
+        }
+        public static class ChatViewHolder extends RecyclerView.ViewHolder {
+            TextView tvChat;
+
+            public ChatViewHolder(View itemView) {
+                super(itemView);
+                tvChat = (TextView) itemView.findViewById(R.id.tvChat);
+            }
+        }
+
     }
 }
