a9f865fd9e3e43741f2c228557bae963da69bc0b
NessCollege <nesscollege2017@gmail.com>
2017-06-27 20:24:24 +0300

    Shared the lists with other users.

--- app/src/main/java/ness/edu/firebasedatabase/dialogs/ShareFragment.java
+++ app/src/main/java/ness/edu/firebasedatabase/dialogs/ShareFragment.java
@@ -146,7 +146,7 @@ public class ShareFragment extends BottomSheetDialogFragment {
                         }
                         if (fragment instanceof DialogFragment)
                             ((DialogFragment) fragment).dismiss();
-                            }
+                    }
                 });
 
             }
